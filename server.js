afe353d6a8399b87ee514173052d89e462c26c38
LenaIT <olena.denysova@gmail.com>
2020-10-08 12:13:21 +0200

    login

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const port = 5000
 //middleware
 app.use(express.json())
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-})
-
 let users = [ 
     {username: "User1", password: "PW1"}, 
     {username: "User2", password: "PW2"}, 
@@ -19,11 +15,29 @@ app.get('/', (req, res) => {
     res.send(users)
 })
 
-app.post("/users", (req,res) => {
+app.post('/users', (req,res) => {
     console.log("post users route called");
     //data from frontend
     console.log(req.body);
     res.send({
         message: 'User logged in successfully...'
     })
+})
+
+app.post('/login', (req, res) => {
+    console.log("post login route called");
+    let name = req.body.name;
+    let password = req.body.password;
+    //const {userName, password} = req.body
+
+    const searchUser = users.find(user => user.username === name && user.password === password);
+    
+    searchUser 
+    ? res.send('Login successful') 
+    : res.send('Login failed')
+
+})
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 })

20be952805d8f76b86ef3e7b43050f72b26b219e
LenaIT <olena.denysova@gmail.com>
2020-10-08 15:10:39 +0200

    routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,3 +6,32 @@ const db = low(adapter)
 db.defaults({
     users: []
 }).write()
+
+exports.getAllUsers = (req, res) => {
+    let users = db.get("users").value()
+    res.send(users)
+}
+
+exports.getUser = (req, res) => {
+    const {id} = req.params
+
+    //find user with given id in json file
+    const user = db.get("users").find({id: id}).value()
+    if(!user)
+        res.send({err: `No user with id ${id}`})
+    else {
+        res.send(user)
+    }
+}
+
+exports.addUser = (req, res) => {
+    const user = req.body 
+    
+    //generate unique id
+    user.id = Date.now().toString()
+
+    //add user to JSON file
+    db.get("users").push(user).write()
+
+    res.send(user)
+}
--- server.js
+++ server.js
@@ -2,21 +2,65 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-const {} = require('./controllers/usersController')
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+})
+
+const {getAllUsers, getUser, addUser} = require('./controllers/usersController')
 
 //middleware
 app.use(express.json())
 
+const hello = (req, res) => {
+    res.send({ welcome: "Welcome to user DB"})
+}
+
+//Routes
+//home page
+app.get('/', hello)
+
+//all users
+app.get('/users',(req, res) => {
+    console.log("get users route called");
+    console.log(req.params);
+    res.send({ users: [{id: "123", name: "Rob"}]})
+})
+
+//sign up a new user
+app.post('/users',(req, res) => {
+    console.log("post route called");
+    console.log(req.body);
+    res.send({ users: [{id: "123", name: "Rob"}]})
+})
+
+//a single user by id
+app.get('/users/:id',(req, res) => {
+    console.log("single user route called");
+    console.log(req.params);
+    res.send({ users: [{id: "123", name: "Rob"}]})
+})
+
+//update existing user by id
+app.patch('/users/:id',(req, res) => {
+    console.log("update route called");
+    console.log(req.params);
+    res.send({ users: [{id: "123", name: "Rob"}]})
+})
+
+//delete existing user by id
+app.delete('/users/:id',(req, res) => {
+    console.log("delete route called");
+    console.log(req.params);
+    res.send({ users: [{id: "123", name: "Rob"}]})
+})
+
+
+/* ex 2
 let users = [ 
     {username: "User1", password: "PW1"}, 
     {username: "User2", password: "PW2"}, 
     {username: "User3", password: "PW3"}
 ]
-
-app.get('/', (req, res) => {
-    res.send(users)
-})
-
 app.post('/users', (req,res) => {
     console.log("post users route called");
     //data from frontend
@@ -38,8 +82,5 @@ app.post('/login', (req, res) => {
     ? res.send('Login successful') 
     : res.send('Login failed')
 
-})
+}) */
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-})
